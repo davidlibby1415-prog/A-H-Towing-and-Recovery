9ae894ec9bde061c70f61dac86e5a29cda86832e
davidlibby1415-prog <davidlibby1415@gmail.com>
2025-11-15 08:25:16 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// postcss.config.js
+const config = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
 };
+
+module.exports = config;
